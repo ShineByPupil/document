60ebebdc2c01da1c1fdfc05171825808d54c6d17
ShineByPupil <522705046@qq.com>
2025-03-09 04:42:55 +0800

    更新配置、修复缺陷

--- docs/examples/base/html/WebComponents/CustomElements.vue
+++ docs/examples/base/html/WebComponents/CustomElements.vue
@@ -4,32 +4,34 @@ import { ref, onMounted } from 'vue';
 const customElementsRef = ref();
 
 onMounted(() => {
-  class UserCard extends HTMLElement {
-    static get observedAttributes() {
-      return ['name', 'age'];
-    } // 声明需监听的属性
+  if (customElements.get('user-card') === undefined) {
+    class UserCard extends HTMLElement {
+      static get observedAttributes() {
+        return ['name', 'age'];
+      } // 声明需监听的属性
 
-    constructor() {
-      super();
-      this.attachShadow({ mode: 'open' }); // 改用 Shadow DOM
-      this.shadowRoot.innerHTML = `
+      constructor() {
+        super();
+        this.attachShadow({ mode: 'open' }); // 改用 Shadow DOM
+        this.shadowRoot.innerHTML = `
       <div class="card">
         <h3></h3>
         <p>Age: <span class="age"></span></p>
       </div>
     `;
-    }
+      }
 
-    attributeChangedCallback(name, oldVal, newVal) {
-      if (name === 'name') {
-        this.shadowRoot.querySelector('h3').textContent = newVal;
-      } else if (name === 'age') {
-        this.shadowRoot.querySelector('.age').textContent = newVal;
+      attributeChangedCallback(name, oldVal, newVal) {
+        if (name === 'name') {
+          this.shadowRoot.querySelector('h3').textContent = newVal;
+        } else if (name === 'age') {
+          this.shadowRoot.querySelector('.age').textContent = newVal;
+        }
       }
     }
-  }
 
-  customElements.define('user-card', UserCard);
+    customElements.define('user-card', UserCard);
+  }
 
   const userCard = document.createElement('user-card');
   userCard.setAttribute('name', 'Bob');
--- docs/examples/base/html/WebComponents/ShadowDOM.vue
+++ docs/examples/base/html/WebComponents/ShadowDOM.vue
@@ -4,13 +4,14 @@ import { ref, onMounted } from 'vue';
 const shadowDOMRef = ref();
 
 onMounted(() => {
-  class ShadowButton extends HTMLElement {
-    constructor() {
-      super();
-      const shadow = this.attachShadow({ mode: 'open' });
+  if (customElements.get('shadow-button') === undefined) {
+    class ShadowButton extends HTMLElement {
+      constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
 
-      // 添加样式和结构
-      shadow.innerHTML = `
+        // 添加样式和结构
+        shadow.innerHTML = `
       <style>
         button {
           padding: 10px 20px;
@@ -22,10 +23,11 @@ onMounted(() => {
       </style>
       <button><slot></slot></button>
     `;
+      }
     }
-  }
 
-  customElements.define('shadow-button', ShadowButton);
+    customElements.define('shadow-button', ShadowButton);
+  }
 
   const shadowButton = document.createElement('shadow-button');
   shadowButton.textContent = '按钮';
