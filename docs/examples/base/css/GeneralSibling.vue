1fcc5e23574e4b8b8cd4e8cdae0d5bedffb3b4fd
ShineByPupil <522705046@qq.com>
2025-03-18 22:44:09 +0800

    更新：核心机制.md

---
+++ docs/examples/base/css/GeneralSibling.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="demo-only-show">
+    <h1>h1</h1>
+    <p>p1</p>
+    <div>div</div>
+    <p>p2</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+h1 ~ p {
+  color: red;
+}
+</style>
